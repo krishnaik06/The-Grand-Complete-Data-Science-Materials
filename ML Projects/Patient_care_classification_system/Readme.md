fda6a8af24c1327c1c37f2b50dd97c61db6d3e3a
YashPurusharthi <104548569+YashPurusharthi@users.noreply.github.com>
2023-12-20 16:51:47 +0000

    added read me file and requirements.txt

---
+++ ML Projects/Patient_care_classification_system/Readme.md
@@ -0,0 +1,36 @@
+# Patient Care Classification System
+
+This project focuses on classifying patients into two categories: "In Care" and "Out Care." The classification is based on various health indicators provided in the dataset. The machine learning models used for classification include Decision Tree, Random Forest, and Logistic Regression.
+
+## Demo Link
+[Streamlit Link](url)
+
+## Dataset
+The dataset, named "PatientCare.csv," contains information about patients, including their gender, age, and various health parameters such as HAEMATOCRIT, HAEMOGLOBINS, ERYTHROCYTE, LEUCOCYTE, THROMBOCYTE, MCH, MCHC, and MCV.
+
+## Preprocessing
+The gender information is mapped to numerical values (1 for Male, 0 for Female) to facilitate model training. Exploratory Data Analysis (EDA) is performed to visualize the distribution of data using count plots and distribution plots.
+
+## Model Training
+The dataset is split into training and testing sets, and three machine learning models are trained: Decision Tree, Random Forest, and Logistic Regression. The accuracy of each model is evaluated using the test set.
+
+## Streamlit Application
+A Streamlit application is created to allow users to input their health parameters. The trained Random Forest model then predicts whether the user requires "In Care" (Hospitalization) or "Out Care" (Home Care). The user input and the model's prediction are displayed in the application.
+
+## Instructions for Running the Streamlit App
+1. Install the required Python libraries: `numpy`, `pandas`, `matplotlib`, `seaborn`, `streamlit`, and `scikit-learn`.
+2. Clone the repository.
+3. Navigate to the project directory in the terminal.
+4. Run the Streamlit app using the command: `streamlit run your_streamlit_app_filename.py`.
+
+## Note
+- The models' performance metrics, including confusion matrices and classification reports, are provided in the Jupyter notebook.
+- Make sure to have the necessary datasets in the specified file path.
+- Adjustments to the dataset path or other parameters may be needed based on your local environment.
+
+Feel free to explore and enhance the project further based on your requirements.
+
+
+
+## Author 
+[LinkedIn Profile](https://www.linkedin.com/in/yashpurusharthi/)
--- ML Projects/Patient_care_classification_system/streamlit_app.py
+++ ML Projects/Patient_care_classification_system/streamlit_app.py
@@ -1,63 +1,77 @@
+
+
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import streamlit as st
-from sklearn.model_selection import train_test_split
+import pickle
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
 
 st.header("Patient Care Classification System")
 
-#read file
-patient_df = pd.read_csv("C:/Users/yashs/Desktop/PatientCare.csv")
+# Load the trained Random Forest model from the pickle file
+with open("/workspaces/The-Grand-Complete-Data-Science-Materials/ML Projects/Patient_care_classification_system/random_forest_model.pkl", "rb") as model_file:
+    rf_model = pickle.load(model_file)
 
-#mapping gender
-gender = {"M" : 1, "F" : 0}
-patient_df["SEX"] = patient_df["SEX"].map(gender)
+# User input
+st_HAEMATOCRIT = st.number_input("Enter HAEMATOCRIT Value: ")
+st_HAEMOGLOBINS = st.number_input("Enter HAEMOGLOBINS Value: ")
+st_ERYTHROCYTE = st.number_input("Enter ERYTHROCYTE Value: ")
+st_LEUCOCYTE = st.number_input("Enter LEUCOCYTE Value: ")
+st_THROMBOCYTE = st.number_input("Enter THROMBOCYTE Value: ")
+st_MCH = st.number_input("Enter MCH Value: ")
+st_MCHC = st.number_input("Enter MCHC Value: ")
+st_MCV = st.number_input("Enter MCV Value: ")
+st_AGE = st.number_input("Enter AGE Value: ")
+st_SEX = st.number_input("Enter SEX (Enter 1 for Male and 0 for Female) Value: ")
 
+user_data = [
+    [
+        st_HAEMATOCRIT,
+        st_HAEMOGLOBINS,
+        st_ERYTHROCYTE,
+        st_LEUCOCYTE,
+        st_THROMBOCYTE,
+        st_MCH,
+        st_MCHC,
+        st_MCV,
+        st_AGE,
+        st_SEX,
+    ]
+]
+cols = [
+    [
+        "HAEMATOCRIT",
+        "HAEMOGLOBINS",
+        "ERYTHROCYTE",
+        "LEUCOCYTE",
+        "THROMBOCYTE",
+        "MCH",
+        "MCHC",
+        "MCV",
+        "AGE",
+        "SEX",
+    ]
+]
 
+pd_test_df = pd.DataFrame(user_data, columns=cols)
 
-df_x = patient_df.iloc[:,0:10]
-df_y = patient_df.iloc[:,10]
-
-x_train, x_test, y_train, y_test=train_test_split(df_x, df_y, test_size=0.20, random_state=1)
-
-rf_model = RandomForestClassifier(random_state = 1)
-
-rf_model.fit(x_train,y_train)
-
-rf_predictions = rf_model.predict(x_test)
-
-st_HAEMATOCRIT = st.number_input("Enter HAEMATOCRIT Value : ")
-st_HAEMOGLOBINS = st.number_input("Enter HAEMOGLOBINS Value : ")
-st_ERYTHROCYTE = st.number_input("Enter ERYTHROCYTE Value : ")
-st_LEUCOCYTE = st.number_input("Enter LEUCOCYTE Value : ")
-st_THROMBOCYTE = st.number_input("Enter THROMBOCYTE Value : ")
-st_MCH = st.number_input("Enter MCH Value : ")
-st_MCHC = st.number_input("Enter MCHC Value : ")
-st_MCV = st.number_input("Enter MCV Value : ")
-st_AGE = st.number_input("Enter AGE Value : ")
-st_SEX = st.number_input("Enter SEX(Enter 1 for Male and 0 for Female) Value : ")
-
-user_data = [[st_HAEMATOCRIT,st_HAEMOGLOBINS,st_ERYTHROCYTE,st_LEUCOCYTE,st_THROMBOCYTE,st_MCH,st_MCHC,st_MCV,st_AGE,st_SEX]]
-cols = [["HAEMATOCRIT","HAEMOGLOBINS","ERYTHROCYTE","LEUCOCYTE","THROMBOCYTE","MCH","MCHC","MCV","AGE","SEX"]]
-
-
-pd_test_df = pd.DataFrame(user_data,columns = cols)
-
-st.subheader('User Input')
+# Display user input
+st.subheader("User Input")
 st.write(pd_test_df)
 
+# Make predictions using the loaded Random Forest model
 rf_predict_user_data = rf_model.predict(pd_test_df)
 
+# Determine the action to be taken based on the prediction
 if rf_predict_user_data == 0:
-    care = 'Out Care(Home Care) Required'
-else :
-    care = 'In Care(Hospitalization) Required'
+    care = "Out Care (Home Care) Required"
+else:
+    care = "In Care (Hospitalization) Required"
 
-st.subheader('Action to Taken')
+# Display the result
+st.subheader("Action to Take")
 st.write(care)
+
 
 
 
