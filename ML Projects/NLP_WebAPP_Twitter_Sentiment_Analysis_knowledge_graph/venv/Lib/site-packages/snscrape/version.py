412bf619584668c8a3c28f0023cead127937f0d0
H10R95 <120493856+H10R95@users.noreply.github.com>
2023-12-30 16:53:25 +0530

    Revert "Revert "End-to-End-Insurance-Premium-Prediction""

--- ML Projects/NLP_WebAPP_Twitter_Sentiment_Analysis_knowledge_graph/venv/Lib/site-packages/snscrape/version.py
+++
@@ -1,7 +0,0 @@
-import importlib.metadata
-
-
-try:
-	__version__ = importlib.metadata.version('snscrape')
-except importlib.metadata.PackageNotFoundError:
-	__version__ = None
--- ML Projects/question-and-answer-system-for-ed-tech-company/chain_retriever/__init__.py
+++
@@ -1,3 +0,0 @@
-from chain_retriever.vector_database_manager import *
-from chain_retriever.qa_retriever import *
-from chain_retriever.models import *
