629bb29775945673b903d1c0dc066d3e0a8b395a
Prathamesh Chaudhary <prathameshchaudhary7122@gmail.com>
2024-02-06 22:41:18 +0530

    car object detection

--- ML Projects/NLP_WebAPP_Twitter_Sentiment_Analysis_knowledge_graph/venv/Lib/site-packages/qtpy/tests/test_qtpdfwidgets.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-def test_qtpdfwidgets():
-    """Test the qtpy.QtPdfWidgets namespace"""
-    QtPdfWidgets = pytest.importorskip("qtpy.QtPdfWidgets")
-
-    assert QtPdfWidgets.QPdfView is not None
